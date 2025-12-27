9d438c0bda3046edefbec3094dc7f0f5a1dd74cf
Marcio Luiz Pedroso <marcioluizpf@icloud.com>
2025-12-27 19:28:40 +0100

    Learning about methos and variables.

--- my-first-java-project/src/App.java
+++ my-first-java-project/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!!");
+        String firstName = "Marcio";
+        String lastName = "Pedroso";
+
+        String fullName = fullName(firstName, lastName);
+        System.out.println(fullName);
+    }
+
+    public static String fullName (String firstName, String lastName) {
+        return "Result of method: " + firstName.concat(" ").concat(lastName);
     }
 }
