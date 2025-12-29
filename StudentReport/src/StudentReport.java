0703a1962d5aa6b11508e796eee8e8c8f2248216
Marcio Luiz Pedroso <marcioluizpf@icloud.com>
2025-12-29 14:19:51 +0100

    StudentReportCard

---
+++ StudentReport/src/StudentReport.java
@@ -0,0 +1,14 @@
+public class StudentReport {
+    public static void main(String[] args) {
+        int finalScore = 85;
+        if (finalScore >= 90) {
+            System.out.println("Grade: A");
+        } else if (finalScore >= 80) {
+            System.out.println("Grade: B");
+        } else if (finalScore >= 70) {
+            System.out.println("Grade: C");
+        } else {
+            System.out.println("Grade: D");
+        }
+    }
+}
