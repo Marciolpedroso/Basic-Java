ec482b3839b5a572c7e7bc982a00b6fe4e501914
marciolpedroso <marcioluizpf@icloud.com>
2025-12-29 21:14:37 +0100

    added primitve types

--- first-steps-in-java/src/marcio/pedroso/firstweek/variables.java
+++ first-steps-in-java/src/marcio/pedroso/firstweek/variables.java
@@ -2,11 +2,17 @@ package marcio.pedroso.firstweek;
 
 public class variables {
     public static void main(String[] args) throws Exception{
+        //Primitive types.
+        
         double minimumWage = 2500;
 
         short shortNumber = 1;
         int normalNumber = shortNumber;
         short shortNumber2 = (short) normalNumber;
-    }
-    
+
+        int number = 5; //That variable can be changed.
+
+        final double PI_VALUE = 3.14; //That variable can't be changed, if you try to change, will receive a console error.
+        //PI_VALUE = 3.15; //You will receive a error, because that variable can't be changed.
+    }   
 }
